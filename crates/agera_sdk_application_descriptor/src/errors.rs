ed2172b2201373656387a10eaed5574e96b59c7f
hydroper <matheusdiasdesouzads@gmail.com>
2023-10-23 07:34:35 -0300

    _

--- crates/agera_sdk_application_descriptor/src/errors.rs
+++ crates/agera_sdk_application_descriptor/src/errors.rs
@@ -1,6 +1,31 @@
 use std::{error::Error, fmt::Display};
 
 #[derive(Debug)]
+pub enum ParsingError {
+    IoError(std::io::Error),
+    JsonError(serde_json::Error),
+    ValidationErrors {
+        invalid_id: Option<InvalidId>,
+    },
+}
+
+impl Display for ParsingError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ParsingError::IoError(error) => error.fmt(f),
+            ParsingError::JsonError(error) => error.fmt(f),
+            ParsingError::ValidationErrors { invalid_id } => {
+                let mut r = vec![];
+                if let Some(error) = invalid_id {
+                    r.push(format!("* {}", error.to_string()));
+                }
+                write!(f, "{}", r.join("\n"))
+            },
+        }
+    }
+}
+
+#[derive(Debug)]
 pub struct InvalidId;
 
 impl Display for InvalidId {
--- crates/agera_sdk_application_descriptor/src/lib.rs
+++ crates/agera_sdk_application_descriptor/src/lib.rs
@@ -1,21 +1,41 @@
+use std::{fs, path::PathBuf};
 use serde::{Deserialize, Serialize};
 use lazy_regex::*;
 
 pub mod errors;
+use self::errors::ParsingError;
 
-#[derive(Deserialize, Serialize)]
+#[derive(Deserialize, Serialize, Debug)]
 pub struct ApplicationDescriptor {
     /// The application ID.
     pub id: String,
 }
 
 impl ApplicationDescriptor {
-    /// Validates the application ID.
-    pub fn validate_id(&self) -> Result<(), errors::InvalidId> {
-        if regex_is_match!(r"[a-z][a-z0-9_]*(\.[a-z][a-z0-9_]*)+", &self.id) {
-            Ok(())
-        } else {
-            Err(errors::InvalidId)
+    /// Parses application descriptor from a project's root directory.
+    pub fn from_project(directory: &str) -> Result<Self, ParsingError> {
+        let descriptor_path = PathBuf::from(directory).join("agera-application.json");
+        let descriptor = fs::read_to_string(descriptor_path);
+        if descriptor.is_err() {
+            return Err(ParsingError::IoError(descriptor.unwrap_err()));
         }
+        let descriptor = serde_json::from_str::<Self>(&descriptor.unwrap());
+        if descriptor.is_err() {
+            return Err(ParsingError::JsonError(descriptor.unwrap_err()));
+        }
+        let descriptor = descriptor.unwrap();
+
+        let invalid_id: Option<errors::InvalidId> = descriptor.validate_id();
+        if invalid_id.is_some() {
+            return Err(ParsingError::ValidationErrors {
+                invalid_id,
+            });
+        }
+
+        Ok(descriptor)
+    }
+
+    fn validate_id(&self) -> Option<errors::InvalidId> {
+        if regex_is_match!(r"[a-z][a-z0-9_]*(\.[a-z][a-z0-9_]*)+", &self.id) { None } else { Some(errors::InvalidId) }
     }
 }
